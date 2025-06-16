31007be4c550186b754c2c9ba06efe771855be14
Henrique bueno <88509491+Henrique-CBueno@users.noreply.github.com>
2025-06-16 18:21:48 -0300

    feat: update environment configuration to support PostgreSQL and SQLite

--- src/database.ts
+++ src/database.ts
@@ -1,17 +1,21 @@
-
-import { knex as setupKnex, Knex} from 'knex'
-import { env } from './env'
+import { knex as setupKnex, Knex } from "knex";
+import { env } from "./env";
 
 export const config: Knex.Config = {
-    client: 'sqlite3',
-    connection: {
-        filename: env.DATABASE_URL,
-    },
-    useNullAsDefault: true,
-    migrations: {
-      extension: 'ts',
-      directory: './DB/migrations'
-    }
-}
+  client: env.DATABASE_CLIENT,
 
-export const knex = setupKnex(config)
+  connection:
+    env.DATABASE_CLIENT === "sqlite"
+      ? {
+          filename: env.DATABASE_URL,
+        }
+      : env.DATABASE_URL,
+
+  useNullAsDefault: true,
+  migrations: {
+    extension: "ts",
+    directory: "./DB/migrations",
+  },
+};
+
+export const knex = setupKnex(config);
